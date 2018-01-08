fc03f373a3cd39f73f75c1e99a73eeea899a491d
Xmonad Jump Virtual Box <xmonad-jumpbox@some.com>
2018-01-08 00:33:44 +0000

    split from blender-scripts repo

---
+++ __init__.py
@@ -0,0 +1,47 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# <pep8 compliant>
+
+bl_info = {
+    "name": "New Planetary System",
+    "author": "",
+    "version": (0, 1),
+    "blender": (2, 79, 0),
+    "location": "View3D > Add > Mesh > New Planetary System",
+    "description": "Adds a new Planetary System",
+    "warning": "",
+    "wiki_url": "",
+    "category": "Add Mesh",
+    }
+
+import bpy
+from . import addon_add_planetary_system
+
+def register():
+    bpy.utils.register_module(__name__)
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
+
+if __name__ == "__main__":
+    register()
+
+
+
--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "# ps-addon" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:inosboxes/ps-addon.git
-git push -u origin master
-
